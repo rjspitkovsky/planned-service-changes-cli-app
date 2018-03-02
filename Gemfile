afc48f91af2fc6c58f986c23168560e19dc68903
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-03-01 22:53:41 -0500

    README.md edit

--- Gemfile
+++ Gemfile
@@ -5,3 +5,4 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 # Specify your gem's dependencies in planned_service_changes_cli_app.gemspec
 gemspec
 gem 'require_all'
+gem 'rake'
--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,12 @@
+
+#lib requirements
 require_relative '../lib/planned_service_changes_cli_app/cli'
+
+
 require "bundler/setup"
 require "planned_service_changes_cli_app"
+
+
+require 'bundler'
+Bundler.require
+require_all './lib'
